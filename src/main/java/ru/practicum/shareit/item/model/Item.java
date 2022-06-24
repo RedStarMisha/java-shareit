88981a795a75f2627d06d7fccfbb7506acf7cf9a
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2022-06-24 10:41:32 +0300

    Init

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,39 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
-import ru.practicum.shareit.requests.model.ItemRequest;
-import ru.practicum.shareit.user.model.User;
-
-import javax.persistence.*;
-
 /**
- * // Item class in storage .
+ * TODO Sprint add-controllers.
  */
-@Getter
-@Setter
-@EqualsAndHashCode(of = {"id"})
-@ToString
-@Entity
-@Table(name = "items")
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "description")
-    private String description;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id")
-    private User owner;
-
-    @Column(name = "available")
-    private Boolean available;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "request_id")
-    private ItemRequest request;
 }
