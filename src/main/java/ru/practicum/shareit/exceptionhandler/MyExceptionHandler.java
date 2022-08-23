17eeb7bb5d0c09a3f877f21086027d010fb6a72d
Misha <borisov.mikhail90@ya.ru>
2022-08-23 23:42:53 +0300

    fixed: мелкие ошибки в ExceptionHandler

--- src/main/java/ru/practicum/shareit/exceptionhandler/MyExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exceptionhandler/MyExceptionHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.exceptionhandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.exceptions.EmailAlreadyExistException;
@@ -29,4 +30,12 @@ public class MyExceptionHandler {
         log.error(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    //обработка исключений валидации
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<String> validateException(MethodArgumentNotValidException e) {
+        log.error(e.getParameter().toString());
+        return new ResponseEntity<>(e.getParameter().getExecutable().toGenericString(), HttpStatus.BAD_REQUEST);
+    }
+
 }
