6aca1aac4c33fccce40d027ba0e2c04a8f1354c7
Misha <borisov.mikhail90@ya.ru>
2022-09-22 15:18:16 +0300

    style: правки по тестам github

--- src/main/java/ru/practicum/shareit/requests/model/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/requests/model/ItemRequestDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.requests.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.dto.ItemDtoShort;
