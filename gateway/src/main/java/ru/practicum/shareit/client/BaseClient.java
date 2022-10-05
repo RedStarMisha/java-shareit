fa6c8150503a5e54b35a60281cc88917f36a30b1
RedstarMisha <borisov.mikhail90@yandex.ru>
2022-10-05 13:57:28 +0300

    feat: подготовил pom файлы

--- gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
