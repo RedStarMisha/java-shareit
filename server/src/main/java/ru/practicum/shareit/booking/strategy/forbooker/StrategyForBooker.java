9285bf1b09d576484ccd765095d7ff31749f0c11
Misha <borisov.mikhail90@ya.ru>
2022-10-09 21:25:53 +0300

    feat: добавил timezone

--- server/src/main/java/ru/practicum/shareit/booking/strategy/forbooker/StrategyForBooker.java
+++ server/src/main/java/ru/practicum/shareit/booking/strategy/forbooker/StrategyForBooker.java
@@ -1,10 +1,6 @@
 package ru.practicum.shareit.booking.strategy.forbooker;
 
-import org.springframework.data.domain.Pageable;
-import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.strategy.Strategy;
-
-import java.util.List;
 
 public interface StrategyForBooker extends Strategy {
 
